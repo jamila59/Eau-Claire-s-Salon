0faa811ac3eb2ca92e17db253820f8921423d1d2
Student <example@example.com>
2020-01-17 08:29:01 -0800

    update naming convention of files.

---
+++ HairSalon/Models/HairSalonContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Models
+{
+  public class HairSalonContext : DbContext
+  {
+
+    public HairSalonContext(DbContextOptions options) : base(options) { }
+  }
+}
